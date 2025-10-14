f3778ab850eb4854ebae89947f3129ca3ee89319
Yin <yinnndlovu@gmail.com>
2025-10-14 16:18:15 +0200

    chore(style): configure code formatting and linting

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:node/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "off",
+  },
+};
