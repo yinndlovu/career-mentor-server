ba1ee483d9d20be38e81df3d0190096cf8faa5e9
Siphelele Miya <siphelelekunene2001@gmail.com>
2025-10-19 20:14:30 +0200

    Update otpRepository.js

--- repositories/otpRepository.js
+++ repositories/otpRepository.js
@@ -2,7 +2,7 @@ const { Otp } = require("../models");
 const OtpTypes = require("../models/enums/otpTypes");
 
 exports.upsertOtp = async (otp, userId, type) => {
-  const expiresAt = new Date(Date.now() + 5 * 60 * 1000);
+  let expiresAt = new Date(Date.now() + 5 * 60 * 1000);
 
   if (type === OtpTypes.EMAIL_VERIFICATION) {
     expiresAt = new Date(Date.now() + 60 * 60 * 1000);
