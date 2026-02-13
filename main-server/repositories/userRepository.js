d9a37344b965c578f676a65c9e8fd64629791ed9
Yin <yin@yinn.dev>
2026-02-13 15:12:37 +0200

    fix: change how database models are imported into the main server

--- main-server/repositories/userRepository.js
+++ main-server/repositories/userRepository.js
@@ -1,4 +1,4 @@
-const { User } = require("../models");
+const { User } = require("@career-mentor/db-core");
 
 exports.createUser = async (fullNames, surname = null, email, password) => {
   const user = await User.create({
@@ -16,6 +16,7 @@ exports.createUser = async (fullNames, surname = null, email, password) => {
     email: user.email,
   };
 };
+
 exports.userHasActiveSubscription =async(userId)=>{
     const user = await User.findByPk(userId);
     const hasSubscription = user.activeSUbscription === true
